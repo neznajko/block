ca7501e6f15cf4869a428c74ab8619da2b51d3cb
4i4mitko <neznajko@yahoo.com>
2024-03-04 15:15:15 +0200

    form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,13 +4,41 @@
 ########################################################
 ########################################################
 class ArticlesController < ApplicationController
+
     def index
+        #
+        # enregisterment                       recording
+    end
+
+    ####################################################
+    def all
+        # 
+        # auf Wiederhören                       good-bye
         @articles = Article.all
+    end
+
+    def new
+        @article = Article.new
     end
 
     def show
         id = params[ :id ]
         @article = Article.find( id )
+    end
+
+    def create
+        @article = Article.new( article_params )
+        if @article.save
+            redirect_to @article
+        else
+            render :new
+        end
+    end
+
+    private
+    def article_params
+        params.require( :article )
+              .permit( :title, :body )
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,16 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
+  ######################################################
   # Defines the root path route ("/")
   root "articles#index"
-  
-  get "/articles",    to: "articles#index"
-  get "/article/:id", to: "articles#show", as: "article"
-
+  ######################################################
+  # bleiben                                      to stay
+  # auflegen                                  to hang up
+  get  "/articles",     to: "articles#all"
+  get  "/articles/new", to: "articles#new",   as: "new_article"
+  get  "/articles/:id", to: "articles#show",  as: "article"
+  post "/articles",     to: "articles#create"
 end
 ########################################################
 # log:
@@ -21,3 +24,7 @@ end
 # 3/01/2024
 # - Bootstrap
 # - link_to
+# 3/04/2024
+# - articles#new
+# - articles#create
+#
