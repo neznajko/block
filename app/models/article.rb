8b3cb8be44259f8f544f01e517cf1b490f59cdb1
neznajko <neznajko@yahoo.com>
2024-10-15 17:35:27 +0300

    Link

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,6 +4,6 @@ class Article < ApplicationRecord
 
   validates_presence_of :title, :body
   validates_length_of :title, maximum: 32 
-  validates_length_of :body, in: 8..120
+  validates_length_of :body, in: 8..1024
 
 end
